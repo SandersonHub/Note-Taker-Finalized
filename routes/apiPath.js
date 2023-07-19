42e186bcb058181305f2f71e7786ab5d1b5d6284
SandersonHub <128574459+SandersonHub@users.noreply.github.com>
2023-07-18 22:00:45 -0400

    New Repo last one broke....

---
+++ routes/apiPath.js
@@ -0,0 +1,47 @@
+const fs = require("fs").promises;
+
+let data;
+
+async function readData() {
+  try {
+    const fileContents = await fs.readFile("./db/db.json", "utf8");
+    data = JSON.parse(fileContents);
+  } catch (error) {
+    console.error("error writing", error);
+    data = [];
+  }
+}
+
+async function writeData() {
+  try {
+    await fs.writeFile("./db/db.json", JSON.stringify(data));
+  } catch (error) {
+    console.error("error", error);
+  }
+}
+
+module.exports = (app) => {
+  readData().catch((error) => console.error("reading initial error", error));
+
+  app.get("/api/notes", (req, res) => {
+    res.json(data);
+  });
+
+  app.get("/api/notes/:id", (req, res) => {
+    const noteId = Number(req.params.id);
+    const note = data.find((note) => note.id === noteId.toString());
+    res.json(note);
+  });
+
+  app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    const uniqueId = data.length.toString();
+    console.log(uniqueId);
+    newNote.id = uniqueId;
+    data.push(newNote);
+
+    writeData().catch((error) => console.error("writing error", error));
+
+    res.json(data);
+  });
+};
---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependents for express and fs
+const express = require("express");
+const fs = require("fs");
+
+// Sets up the express app
+var app = express();
+var PORT = process.env.PORT || 3001
+
+//handles data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/develop/assets", express.static(__dirname + "/develop/assets"));
+
+//Routes to HTML and API path
+require("./routes/HTMLPath")(app);
+require("./routes/apiPath")(app);
+
+//START!!!
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
