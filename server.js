f79c133ae8755b1aa114f4ba12f6514efedc6689
SandersonHub <128574459+SandersonHub@users.noreply.github.com>
2023-07-22 12:31:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-// Importing required modules
 const express = require("express");
-const fs = require("fs");
-
-// Create an instance of express and set up the port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Parse URL-encoded and JSON request bodies
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set up static file serving from the "develop" directory
-app.use(express.static("develop"));
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname + "/develop" });
+});
 
-// Set up routes for HTML and API paths
-require("./routes/HTMLPath")(app);
-require("./routes/apiPath")(app);
+app.use("/api", require("./routes/apiPath"));
 
-// Start the server
 app.listen(PORT, () => {
-    console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT " + PORT);
 });
