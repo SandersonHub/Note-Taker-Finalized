2a416dfdd1256e5e43cf4159e1bc5216c830e9d7
SandersonHub <128574459+SandersonHub@users.noreply.github.com>
2023-07-22 12:42:22 -0400

    added more comment to server.js

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
+// Importing required modules
 const express = require("express");
+//file systen
+const fs = require("fs");
+
+// Create an instance of express and set up the port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Goes through the express app which then turns into data phasing 
+//express.urlencoded is built in middleware which is used to parse the incoming requests
+//extended true, passes to the express.urlended in middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname + "/develop" });
-});
+// Set up static file serving from the "develop" directory
+app.use(express.static("develop"));
 
-app.use("/api", require("./routes/apiPath"));
+// Set up routes for HTML and API paths
+require("./routes/HTMLPath")(app);
+require("./routes/apiPath")(app);
 
+
+//future deployment would to add a delete function
+
+
+// Start the server
 app.listen(PORT, () => {
-  console.log("App listening on PORT " + PORT);
+    console.log("port" + PORT);
 });
